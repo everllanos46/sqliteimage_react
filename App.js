a0a5350f4977cfa9b05ca712074f2a8a629517ce
everllanos46 <62781493+everllanos46@users.noreply.github.com>
2023-08-29 10:34:45 -0500

    first

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+import * as ImagePicker from 'expo-image-picker';
+
+const db = SQLite.openDatabase('mydb.db');
 
 export default function App() {
+  const [imageUri, setImageUri] = useState('');
+  const [imageList, setImageList] = useState([]);
+
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'CREATE TABLE IF NOT EXISTS images (id INTEGER PRIMARY KEY AUTOINCREMENT, uri TEXT);'
+      );
+    });
+    fetchImages();
+  }, []);
+
+  const fetchImages = () => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'SELECT * FROM images;',
+        [],
+        (_, { rows }) => {
+          setImageList(rows._array);
+        }
+      );
+    });
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      db.transaction(tx => {
+        tx.executeSql(
+          'INSERT INTO images (uri) VALUES (?);',
+          [result.uri],
+          () => {
+            fetchImages();
+          },
+          (_, error) => console.error('Error inserting image: ', error)
+        );
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Pick Image" onPress={pickImage} />
+      <View style={styles.imageContainer}>
+        {imageList.map(image => (
+          <Image key={image.id} source={{ uri: image.uri }} style={styles.image} />
+        ))}
+      </View>
     </View>
   );
 }
@@ -13,8 +67,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  imageContainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 10,
   },
 });
